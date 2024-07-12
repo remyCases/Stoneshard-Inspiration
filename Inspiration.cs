b8d394ecc9ad81064f9d9f0fe105e7d49bbb0246
remyCases <remy.cases@gmail.com>
2024-07-12 12:47:46 +0200

    [Major] adds taunt

--- Inspiration.cs
+++ Inspiration.cs
@@ -133,6 +133,133 @@ if(asset_get_index(spell) != o_b_pray && !is_crit && ((damage > 0) || (duration 
             Maneuver: true, 
             Spell: false);
     }
+    private static void AddTaunt()
+    {
+        UndertaleSprite s_taunt = Msl.GetSprite("s_taunt");
+        s_taunt.OriginX = 12;
+        s_taunt.OriginY = 12;
+		UndertaleSprite s_db_taunt = Msl.GetSprite("s_db_taunt");
+        s_db_taunt.OriginX = 12;
+        s_db_taunt.OriginY = 12;	
+
+        UndertaleGameObject taunt_ico = Msl.AddObject("o_skill_taunt_ico", "s_taunt", "o_skill_ico", true, false, true, CollisionShapeFlags.Circle);
+        UndertaleGameObject skill_taunt = Msl.AddObject("o_skill_taunt", "s_taunt", "o_skill", true, false, true, CollisionShapeFlags.Circle);
+        UndertaleGameObject taunt = Msl.AddObject("o_taunt", "s_taunt", "o_target_spell", true, false, true, CollisionShapeFlags.Circle);
+        UndertaleGameObject db_taunt = Msl.AddObject("o_db_taunt", "s_db_taunt", "o_buff_maneuver", true, false, true, CollisionShapeFlags.Circle);
+
+        Msl.AddNewEvent(taunt_ico, @"
+            event_inherited()
+            child_skill = o_skill_taunt
+            event_perform_object(child_skill, ev_create, 0)
+        ", EventType.Create, 0);
+        
+        Msl.AddNewEvent(skill_taunt, @"
+            event_inherited()
+            skill = ""taunt""
+            ds_list_add(attribute, ds_map_find_value(global.attribute, ""STR""), ds_map_find_value(global.attribute, ""WIL""), ds_map_find_value(global.attribute, ""Vitality""))
+            scr_skill_atr()
+        ", EventType.Create, 0);
+        
+        Msl.AddNewEvent(skill_taunt, @"
+            if instance_exists(owner)
+            {
+                ds_map_replace(data, ""CRT"", 60 - (owner.Vitality + owner.WIL))
+                ds_map_replace(data, ""FMB"", 10 + owner.STR)
+                ds_map_replace(data, ""EVS"", - 10 - (owner.Vitality + owner.STR) * 0.5)
+            }
+            event_inherited()
+        ", EventType.Other, 17);
+
+        Msl.AddNewEvent(db_taunt, @"
+            event_inherited()
+            scr_buff_atr()
+            stack = 0
+            first_turn = 1
+            buff_snd = -4
+        ", EventType.Create, 0);
+
+        Msl.AddNewEvent(db_taunt, @"
+            with (target)
+            {
+                ds_list_add(lock_skills, 1)
+            }
+        ", EventType.Alarm, 1);
+        
+        Msl.AddNewEvent(db_taunt, @"
+            event_inherited()
+            with (o_player)
+            {
+                var _crt = 60 - Vitality - WIL
+                var _fmb = 10 + STR
+                var _evs = - 10 - (Vitality + STR) * 0.5
+            }
+            with (target)
+            {
+                ds_map_clear(other.data)
+                ds_map_add(other.data, ""CRT"", _crt)
+                ds_map_add(other.data, ""FMB"", _fmb)
+                ds_map_add(other.data, ""EVS"", _evs)
+                scr_atr_calc(id)
+            }
+        ", EventType.Alarm, 2);
+
+        Msl.AddNewEvent(taunt, @"
+            event_inherited()
+            alarm[10] = 1
+            alarm[0] = 20
+        ", EventType.Create, 0);
+        
+        Msl.AddNewEvent(taunt, @"
+            scr_effect_create(o_db_taunt, 5, target, owner)
+            with (target)
+            {
+                with (scr_guiAnimation(s_sicem_mark, 1, 1, 0))
+                    scr_set_lt(sprite_index)
+            }
+            instance_destroy()
+        ", EventType.Alarm, 0);
+
+        Msl.AddNewEvent(taunt, @"
+            event_inherited()
+            with (owner)
+            {
+                with (scr_guiAnimation(s_sicem_cast, 1, 1, 0))
+                {
+                    scr_set_lt(sprite_index)
+                    scr_audio_play_at(snd_skill_sic_em)
+                }
+            }
+        ", EventType.Alarm, 10);
+
+        Msl.InjectTableSkillsStat(
+            metaGroup: Msl.SkillsStatMetaGroup.MAGICMASTERY,
+            id: "taunt",
+            Object: "o_taunt", 
+            Target: Msl.SkillsStatTarget.TargetObject, 
+            Range: "4", 
+            KD: 30, 
+            MP: 12, 
+            Reserv: 0, 
+            Duration : 0, 
+            AOE_Lenght: 0, 
+            AOE_Width : 0, 
+            is_movement: false, 
+            Pattern: Msl.SkillsStatPattern.normal, 
+            Class : Msl.SkillsStatClass.skill, 
+            Bonus_Range: true, 
+            Starcast: "", 
+            Branch: Msl.SkillsStatBranch.none, 
+            is_knockback: false, 
+            Crime: true, 
+            metacategory: Msl.SkillsStatMetacategory.none, 
+            FMB: 0, 
+            AP: "x", 
+            Attack: false, 
+            Stance: false, 
+            Charge: false, 
+            Maneuver: true, 
+            Spell: false);
+    }
     private static void AddFearsome()
     {
         UndertaleSprite s_fearsome = Msl.GetSprite("s_fearsome");
@@ -191,6 +318,7 @@ if(scr_passive_skill_is_open(o_pass_skill_fearsome, id))
 
         AddPray();
         AddFearsome();
+        AddTaunt();
 
         UndertaleSprite s_weapon_dealer = Msl.GetSprite("s_weapon_dealer");
         s_weapon_dealer.OriginX = 12;
@@ -251,12 +379,13 @@ pop.v.d local.bonus_weapon_price
             new SkillLocation("o_skill_pray_ico", 55, 24),
             new SkillLocation("o_pass_skill_weapon_dealer", 55, 81),
             new SkillLocation("o_pass_skill_corruption", 55, 138),
-            new SkillLocation("o_pass_skill_fearsome", 128, 24)
+            new SkillLocation("o_pass_skill_fearsome", 128, 24),
+            new SkillLocation("o_skill_taunt_ico", 128, 62)
         );
         
         Msl.LoadGML("gml_GlobalScript_scr_skill_tier_init")
             .MatchFrom("}")
-            .InsertBelow("global.inspiration_tier1 = [\"Inspiration\", o_skill_pray_ico, o_pass_skill_weapon_dealer, o_pass_skill_corruption, o_pass_skill_fearsome]")
+            .InsertBelow("global.inspiration_tier1 = [\"Inspiration\", o_skill_pray_ico, o_pass_skill_weapon_dealer, o_pass_skill_corruption, o_pass_skill_fearsome, o_skill_taunt_ico]")
             .Save();
 			
         Msl.LoadGML("gml_GlobalScript_table_Modifiers")
@@ -357,20 +486,17 @@ popz.v";
 
         string id_pray = "o_b_pray";
         string name_pray_en = "Pray";
-
-        Dictionary<ModLanguage, string> name_pray_dict = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {
-           {ModLanguage.English, name_pray_en},
-        });
-        string name_pray = $"\"{id_pray};{string.Join(";", name_pray_dict.Values)}\",";
-
         string desc_pray_en = "Your prayer was answered ! Or maybe you are just more confident about yourself.";
-        Dictionary<ModLanguage, string> desc_pray_dict = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {
-           {ModLanguage.English, desc_pray_en},
-        });
-        string desc_pray = $"\"{id_pray};{string.Join(";", desc_pray_dict.Values)}\",";
 
-        string name = $"{name_pray},";
-        string desc = $"{desc_pray},";
+        string name_pray = $"{id_pray};" + string.Concat(Enumerable.Repeat($"{name_pray_en};", 12));
+        string desc_pray = $"{id_pray};" + string.Concat(Enumerable.Repeat($"{desc_pray_en};", 12));
+
+        string id_taunt = "o_db_taunt";
+        string name_taunt_en = "Taunt";
+        string name_taunt = $"{id_taunt};" + string.Concat(Enumerable.Repeat($"{name_taunt_en};", 12));
+
+        string name = $"\"{name_pray}\",\"{name_taunt}\",";
+        string desc = $"\"{desc_pray}\",";
         
         foreach(string item in input)
         {
@@ -412,19 +538,28 @@ popz.v";
         
         string id_fearsome = "fearsome";
         string name_fearsome_en = "Fearsome";
-        string desc_fearsome_en = "Weak auto-attacks are more likely to reduce the enemies' ~w~will to fight~/~ and ~w~will to fight~/~ losses are increased by ~lg~/*WTFR*/%~/~.";
+        string desc_fearsome_en = "Weak auto-attacks are more likely to reduce the enemies' ~w~will to fight~/~ and enemies' ~w~will to fight~/~ losses are increased by ~lg~/*WTFR*/%~/~.";
 
         Dictionary<ModLanguage, string> name_fearsome = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {{ModLanguage.English, name_fearsome_en},});
         Dictionary<ModLanguage, string> des_fearsome = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {{ModLanguage.English, desc_fearsome_en},});
+        
+        string id_taunt = "taunt";
+        string name_taunt_en = "Taunt";
+        string desc_taunt_en = "Taunt an enemy making him unable to use any abilities. ##Enraged from the taunt, the enemy will receive ~lg~+/*CRT*/%~/~ critical chance, ~r~+/*FMB*/%~/~ fumble and ~r~/*EVS*/%~/~ dodge.";
+
+        Dictionary<ModLanguage, string> name_taunt = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {{ModLanguage.English, name_taunt_en},});
+        Dictionary<ModLanguage, string> des_taunt = Localization.SetDictionary(new Dictionary<ModLanguage, string>() {{ModLanguage.English, desc_taunt_en},});
 
         string name = @$"""{id_pray};{string.Join(";", name_pray.Values)}"",
             ""{id_weapon_dealer};{string.Join(";", name_weapon_dealer.Values)}"",
             ""{id_corruption};{string.Join(";", name_corruption.Values)}"",
-            ""{id_fearsome};{string.Join(";", name_fearsome.Values)}"",";
+            ""{id_fearsome};{string.Join(";", name_fearsome.Values)}"",
+            ""{id_taunt};{string.Join(";", name_taunt.Values)}"",";
         string desc = @$"""{id_pray};{string.Join(";", desc_pray.Values)}"",
             ""{id_weapon_dealer};{string.Join(";", des_weapon_dealer.Values)}"",
             ""{id_corruption};{string.Join(";", des_corruption.Values)}"",
-            ""{id_fearsome};{string.Join(";", des_fearsome.Values)}"",";
+            ""{id_fearsome};{string.Join(";", des_fearsome.Values)}"",
+            ""{id_taunt};{string.Join(";", des_taunt.Values)}"",";
 
         string undead = "\";;///// UNDEAD;///// UNDEAD;;;;;///// UNDEAD;///// UNDEAD;;;;\",";
 
@@ -451,8 +586,13 @@ popz.v";
         string pray_speech = string.Concat(Enumerable.Repeat("Volunt... En!;", 15));
         string pray_end = string.Concat(Enumerable.Repeat("pray_end;", 15));
         
+        string taunt_start = string.Concat(Enumerable.Repeat("taunt;", 15));
+        string taunt_speech = string.Concat(Enumerable.Repeat("COME HERE YOU COWARD !;", 15));
+        string taunt_end = string.Concat(Enumerable.Repeat("taunt_end;", 15));
+        
         string speech = 
-            $"\"{pray_start}\",\"{pray_speech}\",\"{pray_end}\",";
+            @$"""{pray_start}"",""{pray_speech}"",""{pray_end}"",
+            ""{taunt_start}"",""{taunt_speech}"",""{taunt_end}"",";
         
         foreach(string item in input)
         {
